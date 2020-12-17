6e83e4d19d096056fcb162eb5bb15e113d2ca001
Ajay DSouza <me@ajaydsouza.com>
2020-12-17 00:20:40 +0000

    Update options-api.php

--- includes/admin/options-api.php
+++ includes/admin/options-api.php
@@ -161,7 +161,7 @@ function ata_settings_defaults() {
 				$options[ $option['id'] ] = 0;
 			}
 			// If an option is set.
-			if ( in_array( $option['type'], array( 'textarea', 'text', 'csv', 'numbercsv', 'posttypes', 'number' ), true ) && isset( $option['options'] ) ) {
+			if ( in_array( $option['type'], array( 'textarea', 'css', 'html', 'text', 'url', 'csv', 'color', 'numbercsv', 'postids', 'posttypes', 'number', 'wysiwyg', 'file', 'password' ), true ) && isset( $option['options'] ) ) {
 				$options[ $option['id'] ] = $option['options'];
 			}
 			if ( in_array( $option['type'], array( 'multicheck', 'radio', 'select', 'radiodesc', 'thumbsizes' ), true ) && isset( $option['default'] ) ) {
