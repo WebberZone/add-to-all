946b731c4392c5666ecb61a12acbad1b15b127ff
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2023-05-06 19:49:52 +0100

    Update uninstall.php
    Fixes #27

--- uninstall.php
+++ uninstall.php
@@ -10,27 +10,23 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
 
-global $wpdb;
-
 if ( is_multisite() ) {
-
-	// Get all blogs in the network and activate plugin on each one.
-	$blogids = $wpdb->get_col( //phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching
-		"
-		SELECT blog_id FROM $wpdb->blogs
-		WHERE archived = '0' AND spam = '0' AND deleted = '0'
-	"
+	$sites = get_sites(
+		array(
+			'archived' => 0,
+			'spam'     => 0,
+			'deleted'  => 0,
+		)
 	);
 
-	foreach ( $blogids as $blogid ) {
-		switch_to_blog( $blogid );
+	foreach ( $sites as $site ) {
+		switch_to_blog( $site->blog_id );
 		ata_delete_data();
 		restore_current_blog();
 	}
 } else {
 	ata_delete_data();
 }
-
 
 /**
  * Delete Data.
